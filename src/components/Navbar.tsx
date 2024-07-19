ee74e400f463bb181e60015e9aced6793364e69b
fernthanrada <thanrada673@gmail.com>
2024-07-19 10:25:52 +0700

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-lg p-3 mb-5 bg-white rounded">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl"></a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a href="/">หลักสูตรรัฐประศาสนศาตร์</a>
+            </li>
+            <li>
+              <a href="/">หน้าหลัก</a>
+            </li>
+            <li>
+              <a href="/about">เกี่ยวกับ</a>
+            </li>
+            <li>
+              <a href="/ai">AI ตรวจวินิจฉัยโรค</a>
+            </li>
+            <li>
+              <details>
+                <summary>ChatGPT</summary>
+                <ul className="p-2">
+                  <li>
+                    <a>ทีมงาน</a>
+                  </li>
+                  <li>
+                    <a></a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <a>ติดต่อ</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn btn-primary mx-2">Login</a>
+          <a className="btn btn-primary">SignUP</a>
+        </div>
+      </div>
+    </div>
+  );
+}
